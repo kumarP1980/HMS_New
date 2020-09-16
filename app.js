11b5b05165fe0ae2c17f2c95b33c901bd64e913b
kumarP1980 <58350714+kumarP1980@users.noreply.github.com>
2020-09-15 22:59:52 -0400

    Updated Session logic

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require('express-session');
-//const passport = require("passport");
-//const passportLocalMongoose = require("passport-local-mongoose");
-//const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require('mongoose-findorcreate');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+//const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 var placeName="";
@@ -24,8 +23,8 @@ app.use(session({
   saveUninitialized: false
 }));
 
-//app.use(passport.initialize());
-//app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 const url = 'mongodb://localhost:27017';
@@ -33,23 +32,26 @@ mongoose.connect(url + "/HMSDB", { useNewUrlParser: true });
 mongoose.set("useCreateIndex", true);
     
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    streetName: String,
-    city: String,
-    state: String,
-    pincode: String,
-    phoneNumber: String,
-    email: String,
-    userName: String,
-    password: String,
+const userSchema = new mongoose.Schema ({
+    username: String,
+    password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
+//userSchema.plugin(findOrCreate);
+
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+
+passport.deserializeUser(User.deserializeUser());
 
 //Define Schema
 const officeDetail = new mongoose.Schema({
     name: String,
-    userName: String,
+    username: String,
     streetName: String,
     city: String,
     state: String,
@@ -59,37 +61,59 @@ const officeDetail = new mongoose.Schema({
 });
 const OfficeInfo = new mongoose.model("OfficeInfo", officeDetail);
 
-//userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
 
 app.get("/", function (req, res) {
     res.render("home");
 });
 
+app.get("/login", function (req, res) {
+    res.render("login");
+});
 
-app.post("/login", function(req, res){
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+/*app.post("/login", function(req, res){
     const username = req.body.userName;
     const password = req.body.password;    
    
     User.find({$and: [ { userName: { $eq: username } }, { password: { $eq: password } } ]}, function(err, users) {
-       // User.find({}, function(err, users) {
         if (err) {
           console.error(err);
         } else {
-           // const name = users.name;
-           // mongoose.connection.close();
-            // Database Name
-            // const dbName = name;           
-            //mongoose.connect(url + "/" + dbName + "DB", { useNewUrlParser: true });   
             users.forEach(function(users) {
                 placeName = users.userName;             
               });          
             res.redirect("/office");
         }
       });  
+  });*/
+  
+  app.post("/login",function(req,res) {
+      const usrName=req.body.username; 
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user,function(err){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Password:"+req.body.password);
+            passport.authenticate("local")(req, res, function(){
+                placeName = usrName;
+                //console.log("PlaceName:" + placeName);
+                res.redirect("/office");
+                console.log("LOGGED _IN:");
+            });
+        }
+    });
+
   });
 
-  
+ 
 
 /*app.get("/auth/google",
     passport.authenticate('google', { scope: ["profile"] })
@@ -102,16 +126,9 @@ app.post("/login", function(req, res){
         res.redirect("/secrets");
     });*/
 
-app.get("/login", function (req, res) {
-    res.render("login");
-});
 
-app.get("/register", function (req, res) {
-    res.render("register");
-});
 
-app.get("/office", function (req, res) {
-
+/*app.get("/office", function (req, res) {
     const userName = placeName;
     const OfficeInfo = mongoose.model("OfficeInfo", officeDetail);
     OfficeInfo.find({"userName": {$eq: userName}}, function(err, foundUsers){
@@ -124,22 +141,38 @@ app.get("/office", function (req, res) {
         }
       });
 
-});
+});*/
 
-app.get("/submit", function (req, res) {
+app.get("/office", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("submit");
+        const userName = placeName;
+        const OfficeInfo = mongoose.model("OfficeInfo", officeDetail);
+        OfficeInfo.find({"username": {$eq: userName}}, function(err, foundUsers){
+        if (err){
+          console.log(err);
+        } else {
+          if (foundUsers) {
+            res.render("office", {userDetails: foundUsers});
+          }
+        }
+      });
     } else {
         res.redirect("/login");
     }
 });
 
-app.post("/submit", function (req, res) {
+/*app.get("/submit", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});*/
+
+/*app.post("/submit", function (req, res) {
     const submittedSecret = req.body.secret;
 
-    //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
-    // console.log(req.user.id);
-
+    
     User.findById(req.user.id, function (err, foundUser) {
         if (err) {
             console.log(err);
@@ -152,7 +185,7 @@ app.post("/submit", function (req, res) {
             }
         }
     });
-});
+});*/
 
 app.get("/logout", function (req, res) {
    // req.logout();
@@ -160,22 +193,38 @@ app.get("/logout", function (req, res) {
     res.redirect("/");
 });
 
-/*app.post("/register", function (req, res) {
+app.post("/register", function (req, res) {
 
-    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+    User.register({username: req.body.userName },
+        req.body.password, function (err, user) {
         if (err) {
             console.log(err);
             res.redirect("/register");
         } else {
-            passport.authenticate("local")(req, res, function () {
-                res.redirect("/secrets");
+            const office = new OfficeInfo({
+                name: req.body.name,
+                username:req.body.userName,
+                streetName: req.body.streetName,
+                city: req.body.city,
+                state: req.body.state,
+                pincode: req.body.pincode,
+                phoneNumber: req.body.phoneNumber,
+                email: req.body.email
+            });
+            office.save(function (err) {
+                if (!err) {
+                    res.redirect("/login");
+                }
+
             });
         }
     });
 
-});*/
+});
 
-app.post("/register", function (req, res) {
+
+
+/*app.post("/register", function (req, res) {
     const user = new User({
         userName: req.body.userName,
         password: req.body.password,
@@ -191,11 +240,7 @@ app.post("/register", function (req, res) {
     user.save(function (err) {
 
         if (!err) {
-            /*mongoose.connection.close();
-            const dbName = req.body.userName
-            console.log("New DB Name:" + dbName);
-            mongoose.connect(url + "/" + dbName + "DB", { useNewUrlParser: true });
-            mongoose.set("useCreateIndex", true);*/
+            
             
            
             const office = new OfficeInfo({
@@ -223,7 +268,7 @@ app.post("/register", function (req, res) {
     });
 
 
-});
+});*/
 
 
 
