b04520971a3a06ea2ca92e5f98e0d0f13ca4a9ac
kumarP1980 <58350714+kumarP1980@users.noreply.github.com>
2020-09-19 19:35:36 -0400

    Added enrollment, search, logout

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 //const findOrCreate = require('mongoose-findorcreate');
 
+const ageCalc = require("age-calculator");
+let {
+  AgeFromDateString,
+  AgeFromDate
+} = require('age-calculator');
+
 const app = express();
-var placeName="";
+var placeName = "";
+var clinicName = "";
+var personsDetail =[];
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -18,9 +26,9 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(session({
-  secret: "Our little secret.",
-  resave: false,
-  saveUninitialized: false
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
 }));
 
 app.use(passport.initialize());
@@ -30,9 +38,9 @@ app.use(passport.session());
 const url = 'mongodb://localhost:27017';
 mongoose.connect(url + "/HMSDB", { useNewUrlParser: true });
 mongoose.set("useCreateIndex", true);
-    
 
-const userSchema = new mongoose.Schema ({
+
+const userSchema = new mongoose.Schema({
     username: String,
     password: String
 });
@@ -61,9 +69,30 @@ const officeDetail = new mongoose.Schema({
 });
 const OfficeInfo = new mongoose.model("OfficeInfo", officeDetail);
 
+const personDetails = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    username: String,
+    dob:Date,
+    age:Number,
+    streetName: String,
+    city: String,
+    state: String,
+    pincode: String,
+    phoneNumber: String,
+    email: String
+});
+const PersonInfo = new mongoose.model("PersonInfo", personDetails);
+
 
 app.get("/", function (req, res) {
-    res.render("home");
+    if (req.isAuthenticated()) {
+        res.render('office', {
+            clinicName: clinicName
+          });
+    } else {
+        res.render("home");
+    }    
 });
 
 app.get("/login", function (req, res) {
@@ -74,205 +103,142 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
-/*app.post("/login", function(req, res){
-    const username = req.body.userName;
-    const password = req.body.password;    
-   
-    User.find({$and: [ { userName: { $eq: username } }, { password: { $eq: password } } ]}, function(err, users) {
-        if (err) {
-          console.error(err);
-        } else {
-            users.forEach(function(users) {
-                placeName = users.userName;             
-              });          
-            res.redirect("/office");
-        }
-      });  
-  });*/
-  
-  app.post("/login",function(req,res) {
-      const usrName=req.body.username; 
+app.get("/enroll", function (req, res) {    
+    if (req.isAuthenticated()) {
+        res.render('enroll', {
+            clinicName: clinicName
+          });
+    }
+});
+
+app.get("/search", function (req, res) {    
+    if (req.isAuthenticated()) {
+        res.render('search', {
+            clinicName: clinicName,
+            personsDetail: personsDetail
+          });
+    }
+});
+
+app.post("/login", function (req, res) {
     const user = new User({
         username: req.body.username,
         password: req.body.password
     });
 
-    req.login(user,function(err){
-        if(err) {
+    req.login(user, function (err) {
+        if (err) {
             console.log(err);
         } else {
-            console.log("Password:"+req.body.password);
-            passport.authenticate("local")(req, res, function(){
-                placeName = usrName;
-                //console.log("PlaceName:" + placeName);
+            passport.authenticate("local")(req, res, function () {
+                placeName = req.body.username;
                 res.redirect("/office");
-                console.log("LOGGED _IN:");
             });
         }
     });
 
-  });
+});
 
- 
+app.post("/search",function(req, res) {
+    const fname = req.body.fname;
+    const dob = req.body.dob;
+    const phnum = req.body.phoneNumber;    
+    const uname = placeName;
+    if (req.isAuthenticated()) {
+        const PersonInfo = new mongoose.model("PersonInfo", personDetails);
+        PersonInfo.find({ "username": { $eq: uname } , "fname":{ $eq: fname}, "dob":{ $eq: dob}, "phoneNumber":{ $eq: phnum}
+                 }, function (err, foundPerson) {
+            console.log(foundPerson);
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundPerson) {
+                    res.render("search", { personsDetail: foundPerson, clinicName: clinicName });
+                }
+            }
+        });
+    }
 
-/*app.get("/auth/google",
-    passport.authenticate('google', { scope: ["profile"] })
-);*/
 
-/*app.get("/auth/google/secrets",
-    passport.authenticate('google', { failureRedirect: "/login" }),
-    function (req, res) {
-        // Successful authentication, redirect to secrets.
-        res.redirect("/secrets");
-    });*/
+});
 
-
-
-/*app.get("/office", function (req, res) {
-    const userName = placeName;
-    const OfficeInfo = mongoose.model("OfficeInfo", officeDetail);
-    OfficeInfo.find({"userName": {$eq: userName}}, function(err, foundUsers){
-        if (err){
-          console.log(err);
-        } else {
-          if (foundUsers) {
-            res.render("office", {userDetails: foundUsers});
-          }
-        }
-      });
-
-});*/
 
 app.get("/office", function (req, res) {
     if (req.isAuthenticated()) {
         const userName = placeName;
         const OfficeInfo = mongoose.model("OfficeInfo", officeDetail);
-        OfficeInfo.find({"username": {$eq: userName}}, function(err, foundUsers){
-        if (err){
-          console.log(err);
-        } else {
-          if (foundUsers) {
-            res.render("office", {userDetails: foundUsers});
-          }
-        }
-      });
-    } else {
-        res.redirect("/login");
-    }
-});
-
-/*app.get("/submit", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("submit");
-    } else {
-        res.redirect("/login");
-    }
-});*/
-
-/*app.post("/submit", function (req, res) {
-    const submittedSecret = req.body.secret;
-
-    
-    User.findById(req.user.id, function (err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                foundUser.secret = submittedSecret;
-                foundUser.save(function () {
-                    res.redirect("/office");
-                });
-            }
-        }
-    });
-});*/
-
-app.get("/logout", function (req, res) {
-   // req.logout();
-   req.session.destroy();
-    res.redirect("/");
-});
-
-app.post("/register", function (req, res) {
-
-    User.register({username: req.body.userName },
-        req.body.password, function (err, user) {
-        if (err) {
-            console.log(err);
-            res.redirect("/register");
-        } else {
-            const office = new OfficeInfo({
-                name: req.body.name,
-                username:req.body.userName,
-                streetName: req.body.streetName,
-                city: req.body.city,
-                state: req.body.state,
-                pincode: req.body.pincode,
-                phoneNumber: req.body.phoneNumber,
-                email: req.body.email
-            });
-            office.save(function (err) {
-                if (!err) {
-                    res.redirect("/login");
+        OfficeInfo.find({ "username": { $eq: userName } }, function (err, foundUsers) {
+            clinicName = foundUsers[0].name;
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUsers) {
+                    res.render("office", { clinicName: foundUsers[0].name });
                 }
-
-            });
-        }
-    });
-
+            }
+        });
+    } else {
+        res.redirect("/login");
+    }
 });
 
-
-
-/*app.post("/register", function (req, res) {
-    const user = new User({
-        userName: req.body.userName,
-        password: req.body.password,
-        name: req.body.name,
+app.post("/enroll",function(req, res) {
+    let dob = req.body.dob;
+    let age = new AgeFromDateString(dob.toString()).age;
+    const person = new PersonInfo({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        username: placeName,
+        dob: dob,
+        age: age,
         streetName: req.body.streetName,
         city: req.body.city,
         state: req.body.state,
         pincode: req.body.pincode,
         phoneNumber: req.body.phoneNumber,
-        email:req.body.email
+        email: req.body.email
     });
-
-    user.save(function (err) {
-
+    person.save(function (err) {
         if (!err) {
-            
-            
-           
-            const office = new OfficeInfo({
-                name: req.body.name,
-                userName:req.body.userName,
-                streetName: req.body.streetName,
-                city: req.body.city,
-                state: req.body.state,
-                pincode: req.body.pincode,
-                phoneNumber: req.body.phoneNumber,
-                email: req.body.email
-            });
-            office.save(function (err) {
-
-                if (!err) {
-                    res.redirect("/login");
-                }
-
-            });
-
-
-
+            res.redirect("/office");
         }
-
-    });
-
-
-});*/
-
-
-
-
-app.listen(3000, function () {
-    console.log("Server started on port 3000.");
+    });  
 });
+
+app.get("/logout", function (req, res) {
+    req.session.destroy();
+    res.redirect("/");
+});
+
+app.post("/register", function (req, res) {
+
+    User.register({ username: req.body.userName },
+        req.body.password, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect("/register");
+            } else {
+                const office = new OfficeInfo({
+                    name: req.body.name,
+                    username: req.body.userName,
+                    streetName: req.body.streetName,
+                    city: req.body.city,
+                    state: req.body.state,
+                    pincode: req.body.pincode,
+                    phoneNumber: req.body.phoneNumber,
+                    email: req.body.email
+                });
+                office.save(function (err) {
+                    if (!err) {
+                        res.redirect("/login");
+                    }
+
+                });
+            }
+        });
+
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Sever has started on port 3000......");
+  });
