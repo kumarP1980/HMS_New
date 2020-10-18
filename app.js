31eeed95ffe4766a3139242463163cdb8d6d6e8f
kumarP1980 <58350714+kumarP1980@users.noreply.github.com>
2020-10-18 19:07:15 -0400

    Updated Medication, Diagnosis and Contact Details

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 // Required packages
+const expressSanitizer = require('express-sanitizer');
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -12,8 +13,8 @@ const moment = require('moment');
 
 const ageCalc = require("age-calculator");
 let {
-  AgeFromDateString,
-  AgeFromDate
+    AgeFromDateString,
+    AgeFromDate
 } = require('age-calculator');
 
 const app = express();
@@ -21,12 +22,14 @@ const app = express();
 // Global variables
 var placeName = "";
 var clinicName = "";
-var personsDetail =[];
+var personsDetail = [];
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use(expressSanitizer());
 
 // Session Management
 app.use(session({
@@ -51,7 +54,6 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
-//userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
@@ -75,21 +77,37 @@ const officeDetail = new mongoose.Schema({
 const OfficeInfo = new mongoose.model("OfficeInfo", officeDetail);
 
 //Define Person Schema
+var diagnosisData = new mongoose.Schema({
+    details: String,
+    diagnosisDate: String
+});
+
+const DiagnosisData = new mongoose.model("DiagnosisData", diagnosisData);
+
+var medicationData = new mongoose.Schema({
+    name: String, dose: String, startDate: Date, endDate: Date
+})
+
+const MedicationData = new mongoose.model("MedicationData",medicationData);
+
 const personDetails = new mongoose.Schema({
     fname: String,
     lname: String,
     username: String,
-    dob:Date,
-    age:Number,
+    dob: Date,
+    age: Number,
     streetName: String,
     city: String,
     state: String,
     pincode: String,
     phoneNumber: String,
     email: String,
-    insurance:{providerName:String,groupNum:String,policyNum:String,effectiveFrom:Date,providerPhoneNum:String},
-    medication:{name:String,dose:String,startDate:Date,endDate:Date}
+    insurance: { providerName: String, groupNum: String, policyNum: String, effectiveFrom: Date, providerPhoneNum: String },
+    medication: [medicationData],
+    diagnosis: [diagnosisData]
 });
+
+
 const PersonInfo = new mongoose.model("PersonInfo", personDetails);
 
 // Home Route
@@ -97,10 +115,10 @@ app.get("/", function (req, res) {
     if (req.isAuthenticated()) {
         res.render('office', {
             clinicName: clinicName
-          });
+        });
     } else {
         res.render("home");
-    }    
+    }
 });
 
 // Login Route
@@ -116,9 +134,10 @@ app.post("/login", function (req, res) {
 
     req.login(user, function (err) {
         if (err) {
-            console.log(err);
+            console.log("Not valid user");
+            res.render("login");
         } else {
-            passport.authenticate("local")(req, res, function () {
+            passport.authenticate("local", { failureRedirect: '/login' })(req, res, function () {
                 placeName = req.body.username;
                 res.redirect("/office");
             });
@@ -162,15 +181,15 @@ app.post("/register", function (req, res) {
 });
 
 // Define Enroll Route
-app.get("/enroll", function (req, res) {    
+app.get("/enroll", function (req, res) {
     if (req.isAuthenticated()) {
         res.render('enroll', {
             clinicName: clinicName
-          });
+        });
     }
 });
 
-app.post("/enroll",function(req, res) {
+app.post("/enroll", function (req, res) {
     let dob = req.body.dob;
     let age = new AgeFromDateString(dob.toString()).age;
     const person = new PersonInfo({
@@ -185,39 +204,42 @@ app.post("/enroll",function(req, res) {
         pincode: req.body.pinCode,
         phoneNumber: req.body.phoneNumber,
         email: req.body.email,
-        insurance:{providerName:req.body.providerName,
-                   groupNum:req.body.groupNum,
-                   policyNum:req.body.policyNum,
-                   effectiveFrom:req.body.effectiveFrom,
-                   providerPhoneNum:req.body.providerPhoneNum},
+        insurance: {
+            providerName: req.body.providerName,
+            groupNum: req.body.groupNum,
+            policyNum: req.body.policyNum,
+            effectiveFrom: req.body.effectiveFrom,
+            providerPhoneNum: req.body.providerPhoneNum
+        },
 
     });
     person.save(function (err) {
         if (!err) {
             res.redirect("/office");
         }
-    });  
+    });
 });
 
 // Define Search Route
-app.get("/search", function (req, res) {    
+app.get("/search", function (req, res) {
     if (req.isAuthenticated()) {
         res.render('search', {
             clinicName: clinicName,
             personsDetail: personsDetail
-          });
+        });
     }
 });
 
-app.post("/search",function(req, res) {
+app.post("/search", function (req, res) {
     const fname = req.body.fname;
     const dob = req.body.dob;
-    const phnum = req.body.phoneNumber;    
-    const uname = placeName;    
+    const phnum = req.body.phoneNumber;
+    const uname = placeName;
     if (req.isAuthenticated()) {
         const PersonInfo = new mongoose.model("PersonInfo", personDetails);
-        PersonInfo.find({ "username": { $eq: uname } , "fname":{ $eq: fname}, "dob":{ $eq: dob}, "phoneNumber":{ $eq: phnum}
-                 }, function (err, foundPerson) {            
+        PersonInfo.find({
+            "username": { $eq: uname }, "fname": { $eq: fname }, "dob": { $eq: dob }, "phoneNumber": { $eq: phnum }
+        }, function (err, foundPerson) {
             if (err) {
                 console.log(err);
             } else {
@@ -230,45 +252,52 @@ app.post("/search",function(req, res) {
 });
 
 //Define Delete Route
-app.get("/delete/:personID", function(req, res) {
+app.get("/delete/:personID", function (req, res) {
     const requestedPersonId = req.params.personID;
     PersonInfo.findOne({
-      _id: requestedPersonId
-    }, function(err, blogs) {
-      res.render("delete", {
-        //title: blogs.title,
-        //blogText: blogs.blog
-      });
+        _id: requestedPersonId
+    }, function (err, blogs) {
+        res.render("delete", {
+            //title: blogs.title,
+            //blogText: blogs.blog
+        });
     });
-  });
+});
 
-  //Define Edit Route
-  app.get("/edit/:personID", function(req, res) {
+//Define Edit Route
+app.get("/edit/:personID", function (req, res) {
     const requestedPersonId = req.params.personID;
     PersonInfo.findOne({
-      _id: requestedPersonId
-    }, function(err, personDtls) {
+        _id: requestedPersonId
+    }, function (err, personDtls) {
         const time = moment(personDtls.dob);
         const dob = time.format("MM/DD/YYYY");
+        const effFromTime = moment(personDtls.insurance.effectiveFrom);
+        const effFrom = effFromTime.format("MM/DD/YYYY");
         res.render("edit", {
-        id: personDtls._id,  
-        fname: personDtls.fname,
-        lname: personDtls.lname,
-        dob: dob,
-        age: personDtls.age,
-        streetName: personDtls.streetName,
-        city: personDtls.city,
-        state: personDtls.state,
-        pinCode: personDtls.pincode,
-        phoneNumber: personDtls.phoneNumber,
-        email: personDtls.email,
-        clinicName: clinicName
-      });
+            id: personDtls._id,
+            fname: personDtls.fname,
+            lname: personDtls.lname,
+            dob: dob,
+            age: personDtls.age,
+            streetName: personDtls.streetName,
+            city: personDtls.city,
+            state: personDtls.state,
+            pinCode: personDtls.pincode,
+            phoneNumber: personDtls.phoneNumber,
+            email: personDtls.email,
+            clinicName: clinicName,
+            providerName:personDtls.insurance.providerName,
+            groupNum:personDtls.insurance.groupNum,
+            policyNum:personDtls.insurance.policyNum,
+            effFrom:effFrom,
+            providerPhone:personDtls.insurance.providerPhoneNum
+        });
     });
-  });
+});
 
-  //Define Modify Route
-  app.post("/modify", function(req,res) {
+//Define Modify Route
+app.post("/modify", function (req, res) {
     let personID = req.body.personID;
     let streetName = req.body.streetName;
     let city = req.body.city;
@@ -279,44 +308,48 @@ app.get("/delete/:personID", function(req, res) {
     const filter = { _id: personID };
     const update = { streetName: streetName, city: city, state: state, pincode: pinCode, phoneNumber: phoneNumber, email: email };
     mongoose.set('useFindAndModify', false);
-    PersonInfo.findOneAndUpdate(filter, update, function (err, person) {        
-      });
+    PersonInfo.findOneAndUpdate(filter, update, function (err, person) {
+    });
     if (req.isAuthenticated()) {
         res.redirect("/search");
     }
-  });
+});
 
-  //Define view Route
-app.get("/view/:personID", function(req, res) {
+//Define view Route
+app.get("/view/:personID", function (req, res) {
     const requestedPersonId = req.params.personID;
+    let diagnosisDtls = [];
     PersonInfo.findOne({
-      _id: requestedPersonId
-    }, function(err, personDtls) {        
-        var areaCode = (personDtls.phoneNumber).substr(0,3);
-        var phoneNum = (personDtls.phoneNumber).substr(3,9);   
+        _id: requestedPersonId
+    }, function (err, personDtls) {
+        var areaCode = (personDtls.phoneNumber).substr(0, 3);
+        var phoneNum = (personDtls.phoneNumber).substr(3, 9);
         const effFromTime = moment(personDtls.insurance.effectiveFrom);
         const effFrom = effFromTime.format("MM/DD/YYYY");
+        console.log("Medications:" + personDtls.medication);
         res.render("view", {
-        id: personDtls._id,  
-        fname: personDtls.fname,
-        lname: personDtls.lname,
-        age: personDtls.age,
-        streetName: personDtls.streetName,
-        city: personDtls.city,
-        state: personDtls.state,
-        pinCode: personDtls.pincode,
-        areaCode:areaCode,
-        phoneNumber: phoneNum,
-        email: personDtls.email,
-        clinicName: clinicName,
-        providerName:personDtls.insurance.providerName,
-        groupNum:personDtls.insurance.groupNum,
-        policyNum:personDtls.insurance.policyNum,
-        effectiveFrom:effFrom,
-        providerPhone:personDtls.insurance.providerPhoneNum
-      });
+            id: personDtls._id,
+            fname: personDtls.fname,
+            lname: personDtls.lname,
+            age: personDtls.age,
+            streetName: personDtls.streetName,
+            city: personDtls.city,
+            state: personDtls.state,
+            pinCode: personDtls.pincode,
+            areaCode: areaCode,
+            phoneNumber: phoneNum,
+            email: personDtls.email,
+            clinicName: clinicName,
+            providerName: personDtls.insurance.providerName,
+            groupNum: personDtls.insurance.groupNum,
+            policyNum: personDtls.insurance.policyNum,
+            effectiveFrom: effFrom,
+            providerPhone: personDtls.insurance.providerPhoneNum,            
+            diagnosisDtls:personDtls.diagnosis,
+            medicationDtls:personDtls.medication
+        });
     });
-  });
+});
 
 //Define Office Route
 app.get("/office", function (req, res) {
@@ -338,12 +371,142 @@ app.get("/office", function (req, res) {
     }
 });
 
+//Define Contacts Route
+app.get("/contact", function (req, res) {
+    if (req.isAuthenticated()) {
+        const userName = placeName;
+        const OfficeInfo = mongoose.model("OfficeInfo", officeDetail);
+        OfficeInfo.find({ "username": { $eq: userName } }, function (err, foundUsers) {           
+            var areaCode = (foundUsers[0].phoneNumber).substr(0, 3);
+            var phoneNum = (foundUsers[0].phoneNumber).substr(3, 9);
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUsers) {
+                    res.render("contact", { 
+                        clinicName: foundUsers[0].name,
+                        areaCode: areaCode,
+                        phoneNumber: phoneNum,
+                        email: foundUsers[0].email,
+                        streetName:foundUsers[0].streetName,
+                        city:foundUsers[0].city,
+                        state:foundUsers[0].state,
+                        pinCode:foundUsers[0].pincode
+                     });
+                }
+            }
+        });
+    } else {
+        res.redirect("/login");
+    }
+});
+
 //Define logout Route
 app.get("/logout", function (req, res) {
     req.session.destroy();
     res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+// Define Medication Route
+app.get("/medication/:personID", function (req, res) {
+    const requestedPersonId = req.params.personID;
+    if (req.isAuthenticated()) {
+        res.render('medication', {
+            clinicName: clinicName,
+            id: requestedPersonId
+        });
+    }
+});
+
+app.post("/medication", function (req, res) {
+    let personID = req.body.personID;
+    let medSize = ((req.body.meds).length);  
+ 
+    for (i=0;i<medSize;i++) {
+        let medName = req.body.meds[i].medName;
+        let dose = req.body.meds[i].dose;
+        let startDt = req.body.meds[i].startDate;
+        let endDt = req.body.meds[i].endDate;
+        const medicationInfo = new MedicationData({
+        name: medName,
+        dose: dose,
+        startDate:startDt,
+        endDate:endDt
+    });
+    medicationInfo.save(function (err) {
+    });
+    console.log("Value of i:" + i);
+    console.log(medicationInfo);
+    const filter = { _id: personID};
+    mongoose.set('useFindAndModify', false);
+    PersonInfo.findById(filter, function (err, person) {
+        person.medication.push(medicationInfo);
+        person.save(function(err){
+            // no err here!
+            if(err) {
+                console.log(err);
+            }    
+        }); 
+    }); 
+    console.log("Data Save in Person");
+    }
+    if (req.isAuthenticated()) {
+        res.redirect("/view/" + personID);
+    } 
+        
+});
+
+
+// Define Diagnosis route
+app.get("/diagnosis/:personID", function (req, res) {
+    const requestedPersonId = req.params.personID;
+    if (req.isAuthenticated()) {
+        PersonInfo.findOne({
+            _id: requestedPersonId
+        }, function (err, personDtls) {
+            res.render("diagnosis", {
+                id: personDtls._id,
+                fname: personDtls.fname,
+                lname: personDtls.lname,
+                clinicName: clinicName
+            });
+        });
+    }
+});
+
+app.post("/diagnosis", function (req, res) {
+    let personID = req.body.personID;
+    let options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    let today = new Date().toLocaleDateString("en-US", options);
+    let diagnosis = req.sanitize(req.body.body.content);
+    const diagnosisInfo = new DiagnosisData({
+        details: diagnosis,
+        diagnosisDate: today 
+    });
+    diagnosisInfo.save(function (err) {
+    });
+    const filter = { _id: personID};
+    const update = { diagnosis: diagnosisInfo };
+    mongoose.set('useFindAndModify', false);
+    PersonInfo.findById(filter, function (err, person) {
+        person.diagnosis.push(diagnosisInfo);
+        person.save(function(err){
+            // no err here!
+            if(err) {
+                console.log(err);
+            } else if (req.isAuthenticated()) {
+                res.redirect("/view/" + personID);
+            }    
+        });   
+    }); 
+    
+    
+});
+
+app.listen(process.env.PORT || 3000, function () {
     console.log("Sever has started on port 3000......");
-  });
+});
